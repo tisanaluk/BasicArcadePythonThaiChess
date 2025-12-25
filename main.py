7ed6bcf34a8ddd739d49f12fde0891ead7b24155
tisanaluk <38757207+tisanaluk@users.noreply.github.com>
2025-12-25 18:39:41 +0700

    Create MainView class for arcade application
    Implement main application view with UI manager and button.

---
+++ main.py
@@ -0,0 +1,47 @@
+
+import arcade
+import arcade.gui
+
+# Screen title and size
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Making a Menu"
+
+
+class MainView(arcade.View):
+    """ Main application class."""
+
+    def __init__(self):
+        super().__init__()
+        self.manager = arcade.gui.UIManager()
+        
+        switch_button = arcade.gui.UIFlatButton(text="Pause", width=200)
+
+    def on_show_view(self):
+        """ This is run once when we switch to this view """
+        arcade.set_background_color(arcade.color.DARK_BLUE_GRAY)
+        
+        self.manager.enable()
+
+    def on_draw(self):
+        """ Render the screen. """
+        # Clear the screen
+        self.clear()
+        
+        # draw the ui manager
+        self.manager.draw()
+
+    def on_hide_view(self):
+        # Disable the UIManager when the view is hidden.
+        self.manager.disable()
+
+
+def main():
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, resizable=True)
+    main_view = MainView()
+    window.show_view(main_view)
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
